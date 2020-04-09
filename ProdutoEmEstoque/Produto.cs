b92367226a8067a35ea777e72c02aa0f51b80747
Igor Gaspar <igor.elvis@hotmail.com>
2020-04-09 16:51:15 -0300

    Programa que faz o estoque de uma loja, podendo adicionar ou remover itens.

---
+++ ProdutoEmEstoque/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ProdutoEmEstoque {
+    class Produto {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double valorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade) {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString() {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + valorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
